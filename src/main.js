8560d155eda79a8488ab4076c5e1c81a1d2c4598
Artur <artur.muron6@gmail.com>
2019-07-15 17:20:08 +0200

    vue masonry added

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import App from './App.vue'
 import router from './router';
 import BootstrapVue from 'bootstrap-vue'
 import vueHeadful from 'vue-headful';
+import {VueMasonryPlugin} from 'vue-masonry';
 
-Vue.component('vue-headful', vueHeadful);
+Vue.use(VueMasonryPlugin)
 
 import VueMasonry from 'vue-masonry-css'
 
 Vue.use(VueMasonry);
+
+Vue.component('vue-headful', vueHeadful);
+
 
 Vue.use(BootstrapVue)
 
