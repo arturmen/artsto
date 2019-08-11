44544f4b3ad44bd43c974fd59039a7ae0012694e
Artur <artur.muron6@gmail.com>
2019-08-11 16:54:20 +0200

    direct link to gallery

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -5,14 +5,14 @@
                 <a
                 id="logo"
                 href="/#/"><img src="../assets/images/logo/logo-artsto-dark.png"/></a>
-                <a class="pt-4 pr-5 float-right menu-big" href="/#/gallery">GALERIA</a>
+                <a class="pt-4 pr-5 float-right menu-big" href="artstos.vot.pl/gallery.html">GALERIA</a>
                 <a class="pt-4 pr-5 float-right menu-big" href="/#/products">PRODUKTY</a>
                 <a class="pt-4 pr-5 float-right menu-big" href="/#/">STRONA GŁÓWNA</a>
                 <div class="menu-small" style="margin: 0 auto; position: relative; left: 25%; max-width: fit-content">
                 <b-dropdown size="lg" right variant="link" toggle-class=" text-center text-decoration-none" no-caret>
                     <template slot="button-content"> <font-awesome-icon class="pt-3 menu-small dropdown b-dropdown" style=" margin: 0 auto; font-size: 24px; -webkit-box-sizing: content-box!important; box-sizing: content-box!important" icon="bars" /></template>
                     <b-dropdown-item href="/#/">STRONA GŁÓWNA</b-dropdown-item>
-                    <b-dropdown-item href="/#/gallery">GALERIA</b-dropdown-item>
+                    <b-dropdown-item href="artstos.vot.pl/gallery.html">GALERIA</b-dropdown-item>
                     <b-dropdown-item href="/#/products">PRODUKTY</b-dropdown-item>
                 </b-dropdown>
                 </div>
